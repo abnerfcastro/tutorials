4247469d7f8ce1e8fe43bcb7a5943f33f34d7830
Abner Castro <abnerfcastro@gmail.com>
2015-03-13 01:46:26 -0300

    Implemented user name prompting.

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
+	std::string name;
+
 	// This is a simple program
 	std::cout << "Hi! How are you?" << " ";
 	std::cout << "This is a tutorial with Git" << std::endl;
+	std::cout << "Tell us your name: ";
+	std::cin >> name;
+	std::cout << "Thank you!" << std::endl;
+	
 	return 0;
 }
