1bb0b238ac3c9922547f1e413f35d25b2db66cd9
Abner Castro <abnerfcastro@gmail.com>
2015-03-13 02:10:58 -0300

    Everything is fixed.

--- hello.cpp
+++ hello.cpp
@@ -3,15 +3,10 @@
 
 int main()
 {
-<<<<<<< HEAD
-	std::cout << "Hi! How are you?" << std::endl;
-	std::cout << "Hello World!" << std::endl;
-=======
 	std::string name;
 
 	// This is a simple program
 	std::cout << "Hi! How are you?" << " ";
->>>>>>> my-temporary-work
 	std::cout << "This is a tutorial with Git" << std::endl;
 	std::cout << "Tell us your name: ";
 	std::cin >> name;
